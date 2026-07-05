b5d4115aead77701cae3bfae06cd245bf1def1d7
Lipaev Alexei <alexeijan2@gmail.com>
2026-07-05 09:10:51 +0300

    fix a bug when the button doesn't show up after loading page

--- content.js
+++ content.js
@@ -2,11 +2,15 @@ let linkObject = document.createElement("button")
 linkObject.addEventListener("click", () => window.scrollTo(0, 0))
 linkObject.textContent = "TOP"
 linkObject.id = "topExtensionButton"
-document.addEventListener("scroll", () => {
-    let exist = document.querySelector("#topExtensionButton")
+let exist;
+up_button();
+document.addEventListener("scroll", up_button)
+
+function up_button() {
+    exist = document.querySelector("#topExtensionButton")
     if (window.scrollY >= window.innerHeight / 2 && !exist) {
         document.body.append(linkObject)
     } else if (window.scrollY <= window.innerHeight / 2 && exist) {
         exist.remove()
     }
-})
+}
